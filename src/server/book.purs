fd7992c509cef18cc70d9ae8911f9faaa2dd0dab
Nicholas Kariniemi <nicholas.kariniemi@gmail.com>
2015-05-28 11:16:42 +0300

    Separate books into own module

---
+++ src/server/book.purs
@@ -0,0 +1,40 @@
+module DeanList.Server.Book where 
+
+import Data.Array (mapMaybe, (!!))
+import Data.JSON (JValue(..), JObject(..), JArray(..), ToJSON, encode, decode, object)
+import Data.Map (lookup, toList)
+import Data.Maybe (Maybe(Just, Nothing))
+import Data.Tuple (Tuple(..), uncurry)
+
+data Book = Book {title :: String, url :: String}
+
+instance bookToJSON :: ToJSON Book where
+  toJSON (Book b) = object [Tuple "title" (JString b.title), Tuple "url" (JString b.url)]
+
+asObject :: JValue -> Maybe JObject
+asObject (JObject j) = (Just j)
+asObject _ = Nothing
+
+asString :: JValue -> Maybe String
+asString (JString s) = (Just s)
+asString _ = Nothing
+
+parseBook :: String -> JValue -> Maybe Book
+parseBook title (JArray bookInfoArray) = do
+  fileInfo <- (bookInfoArray !! 1)
+  fileInfoObj <- asObject fileInfo
+  bookUrl <- lookup "ro_uri" fileInfoObj
+  bookUrl' <- asString bookUrl
+  return $ Book {title: title, url: bookUrl'}
+parseBook _ _ = Nothing
+    
+parseBooks :: String -> Maybe [Book]
+parseBooks response = do
+  json <- decode response :: Maybe JArray
+  dirInfo <- (json !! 1)
+  dirInfoObj <- asObject dirInfo
+  children <- lookup "children" dirInfoObj
+  childrenObj <- asObject children
+  childrenList <- Just $ toList childrenObj
+  books <- Just $ mapMaybe (uncurry parseBook) childrenList
+  return books
--- src/server/main.purs
+++ src/server/main.purs
@@ -1,70 +1,31 @@
 module DeanList.Server.Main where 
 
-import Control.Apply (lift2)
+import DeanList.Server.Book (Book(Book), parseBooks)
+
 import Control.Monad.Eff (Eff(..))
 import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Exception (Error(..), message)
-import Control.MonadPlus (guard)
 import Debug.Trace (trace, print)
-import Data.Array (map, catMaybes, mapMaybe, (!!))
-import Data.Foldable (foldl)
 import Data.Function (Fn3(..))
 import Data.Foreign.EasyFFI (unsafeForeignFunction)
-import Data.JSON (JValue(..), JObject(..), JArray(..), ToJSON, encode, decode, object, Pair(..))
-import Data.Map (Map(..), lookup, toList, fromList)
-import Data.Maybe (Maybe(Just, Nothing), maybe, isJust, fromMaybe)
-import Data.Tuple (Tuple(..), uncurry)
-import Network.HTTP.Affjax (AJAX(..), affjax, defaultRequest)
-import Network.HTTP.Method (Method(GET))
+import Data.JSON (encode)
+import Data.Maybe (fromMaybe)
 import Node.Encoding (Encoding(UTF8))
 import Node.Express.App (App(..), listenHttp, use, useExternal, useOnError, get)
-import Node.Express.Handler (Handler(..), setStatus, send, sendJson, getOriginalUrl, next, capture)
+import Node.Express.Handler (Handler(..), setStatus, sendJson, getOriginalUrl, next)
 import Node.Express.Types (Request(..), Response(..), ExpressM(..))
-import Node.FS.Sync (readdir, readTextFile)
+import Node.FS.Sync (readTextFile)
 
 foreign import staticMiddleware "var staticMiddleware = require('express').static"
     :: String -> Fn3 Request Response (ExpressM Unit) (ExpressM Unit)
 
-data Book = Book {title :: String, url :: String}
-
-instance bookToJSON :: ToJSON Book where
-  toJSON (Book b) = object [Tuple "title" (JString b.title), Tuple "url" (JString b.url)]
-
 booksUrl = "http://localhost:3456/uri/URI%3ADIR2%3Arilot7zn3ycl6lmngkd5iab3pm%3Ahzko5cxtvwevu33qbyv72b3mn5tfzl7l7igllceqiax6akkc6clq/?t=json"
-
-asObject :: JValue -> Maybe JObject
-asObject (JObject j) = (Just j)
-asObject _ = Nothing
-
-asString :: JValue -> Maybe String
-asString (JString s) = (Just s)
-asString _ = Nothing
 
 logger :: Handler
 logger = do
     url <- getOriginalUrl
     liftEff $ trace url
     next
-    
-parseBook :: String -> JValue -> Maybe Book
-parseBook title (JArray bookInfoArray) = do
-  fileInfo <- (bookInfoArray !! 1)
-  fileInfoObj <- asObject fileInfo
-  bookUrl <- lookup "ro_uri" fileInfoObj
-  bookUrl' <- asString bookUrl
-  return $ Book {title: title, url: bookUrl'}
-parseBook _ _ = Nothing
-    
-parseBooks :: String -> Maybe [Book]
-parseBooks response = do
-  json <- decode response :: Maybe JArray
-  dirInfo <- (json !! 1)
-  dirInfoObj <- asObject dirInfo
-  children <- lookup "children" dirInfoObj
-  childrenObj <- asObject children
-  childrenList <- Just $ toList childrenObj
-  books <- Just $ mapMaybe (uncurry parseBook) childrenList
-  return books
     
 getBooks :: Handler
 getBooks = do
